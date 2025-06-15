517e45080dc93d4abf0a69255d21f1ac054c25c1
Himanshujadhav2004 <himanshujadhav341@gmail.com>
2025-06-15 23:59:12 +0530

    Initial commit for frontend

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId = "18097c4b1e76c713968efc0d0b55d23f";
 
 if (!clientId) {
   throw new Error("No client ID provided");
